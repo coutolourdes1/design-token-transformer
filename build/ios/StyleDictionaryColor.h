680c64b8bc8b40ba5e807595f923bcb67216b369
coutolourdes1 <coutolourdes1@users.noreply.github.com>
2022-04-28 16:12:31 +0000

    Tokens updated

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Thu, 28 Apr 2022 16:12:30 GMT
 
 
 #import <UIKit/UIKit.h>
@@ -13,11 +13,8 @@ typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
 ColorColorsMultipleFills0,
 ColorColorsMultipleFills1,
 ColorColorsSingleBlue,
-ColorColorsRefBlue,
 ColorColorsSpecialCharacters,
-ColorColorsSpecialCharactersNderung,
-ColorLightBackground,
-ColorDarkBackground
+ColorColorsSpecialCharactersNderung
 };
 
 @interface StyleDictionaryColor : NSObject
